c85b14b595dfcf438c156965a84bf94023ace85b
hoco1 <mohammadrezanorouzi600@gmail.com>
2022-12-22 20:56:56 +0330

    login & signup user

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI,Body,Depends
 from app.db import conn
 from app.schemas import serializeDict,serializeList
-from app.model import InstagramSchema
+from app.model import InstagramSchema,UserLoginSchema,UserSchema
 from app.scraping import *
 from bson import ObjectId
+from app.auth.jwt_handler import signJWT
+from app.auth.jwt_bearer import jwtBearer
 
 app = FastAPI()
 # Get - for testing
@@ -36,3 +38,32 @@ def add_cookie(instagram:InstagramSchema):
     cookies = login_response.cookies.get_dict()
     conn.local.cookie.insert_one(dict(cookies))
     return serializeList(conn.local.cookie.find())
+
+# user Signup [Create a new user]
+@app.post("/user/signup",tags=["user"])
+def user_signup(user:UserSchema=Body(default=None)):
+    conn.local.user.insert_one(dict(user))
+    return serializeList(conn.local.user.find())
+
+def check_user(data:UserLoginSchema):
+    db_user = conn.local.user.find_one({"email":data.email.lower()})
+    
+    if not db_user:
+        return False
+    
+    user = serializeDict(db_user)
+    
+    if not user["password"] == data.password:
+        return False
+    
+    return True
+    
+
+@app.post("/user/login",tags=["user"])
+def user_login(user:UserLoginSchema=Body(default=None)):
+    if check_user(user):
+        return signJWT(user.email)
+    else:
+        return{
+            "error":"Invalid login details!"
+        }
